621df3b550a8c37093dcb5ad455e9f60d26ce34e
Bob Nystrom <robert@stuffwithstuff.com>
2016-04-29 07:04:50 -0700

    Reuse code better in table.

--- c/table.c
+++ c/table.c
@@ -31,47 +31,47 @@ static void growTable(Table* table) {
   table->entries = REALLOCATE(table->entries, TableEntry, table->capacity);
 }
 
-bool tableGet(Table* table, ObjString* key, Value* value) {
+TableEntry* findEntry(Table* table, const uint8_t* chars, int length) {
   // TODO: Actually hash it!
   for (int i = 0; i < table->count; i++) {
     TableEntry* entry = &table->entries[i];
-    if (entry->key == key) {
-      *value = table->entries[i].value;
-      return true;
+    if (entry->key->length == length &&
+        memcmp(entry->key->chars, chars, length) == 0) {
+      return entry;
     }
   }
   
-  return false;
+  return NULL;
+}
+
+bool tableGet(Table* table, ObjString* key, Value* value) {
+  TableEntry* entry = findEntry(table, key->chars, key->length);
+  if (entry == NULL) return false;
+  
+  *value = entry->value;
+  return true;
 }
 
 bool tableSet(Table* table, ObjString* key, Value value) {
-  // TODO: Actually hash it!
-  for (int i = 0; i < table->count; i++) {
-    TableEntry* entry = &table->entries[i];
-    if (entry->key == key) {
-      table->entries[i].value = value;
-      return true;
-    }
+  // Reuse the existing entry if the key is already present.
+  TableEntry* entry = findEntry(table, key->chars, key->length);
+  if (entry != NULL) {
+    entry->value = value;
+    return true;
   }
   
   growTable(table);
-  TableEntry* entry = &table->entries[table->count++];
+  entry = &table->entries[table->count++];
   entry->key = key;
   entry->value = value;
   return false;
 }
 
 ObjString* tableFindKey(Table* table, const uint8_t* chars, int length) {
-  // TODO: Actually hash it!
-  for (int i = 0; i < table->count; i++) {
-    TableEntry* entry = &table->entries[i];
-    if (entry->key->length == length &&
-        memcmp(entry->key->chars, chars, length) == 0) {
-      return entry->key;
-    }
-  }
+  TableEntry* entry = findEntry(table, chars, length);
+  if (entry == NULL) return NULL;
   
-  return NULL;
+  return entry->key;
 }
 
 void tableRemoveWhite(Table* table) {
