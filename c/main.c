02f21c0b45bc4a1b8b80d0dde499b482f8d4f8d0
Bob Nystrom <robert@stuffwithstuff.com>
2018-05-07 13:35:12 -0700

    Second draft of "Scanning on Demand".

--- c/main.c
+++ c/main.c
@@ -70,7 +70,7 @@ static char* readFile(const char* path) {
 static void runFile(const char* path) {
   char* source = readFile(path);
   InterpretResult result = interpret(source);
-  free(source);
+  free(source); // [owner]
 
   if (result == INTERPRET_COMPILE_ERROR) exit(65);
   if (result == INTERPRET_RUNTIME_ERROR) exit(70);
