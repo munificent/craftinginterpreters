4839c884c3aaf0e54a53182d105932428971b8a0
Bob Nystrom <robert@stuffwithstuff.com>
2018-06-17 21:48:39 -0700

    Explain exit codes.
    Fix #241.

--- java/com/craftinginterpreters/lox/Lox.java
+++ java/com/craftinginterpreters/lox/Lox.java
@@ -23,6 +23,7 @@ public class Lox {
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
+      System.exit(64); // [64]
     } else if (args.length == 1) {
       runFile(args[0]);
     } else {
