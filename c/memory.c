c63d53a5bf965efd92721c91760311605a79bd83
Robert Nystrom <robert@stuffwithstuff.com>
2019-11-23 15:20:30 -0800

    Fix #519.

--- c/memory.c
+++ c/memory.c
@@ -181,7 +181,7 @@ static void freeObject(Obj* object) {
     case OBJ_CLOSURE: {
 //> free-upvalues
       ObjClosure* closure = (ObjClosure*)object;
-      FREE_ARRAY(ObjUpvalue, closure->upvalues, closure->upvalueCount);
+      FREE_ARRAY(ObjUpvalue*, closure->upvalues, closure->upvalueCount);
 //< free-upvalues
       FREE(ObjClosure, object);
       break;
