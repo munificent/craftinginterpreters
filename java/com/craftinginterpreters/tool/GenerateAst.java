c568e6f2829cf838ea3dd06dc1e51e4e1e7b00a0
Max Brauer <mamachanko@users.noreply.github.com>
2020-04-12 14:19:59 +0200

    use exit code 64 consistently for improper usage

--- java/com/craftinginterpreters/tool/GenerateAst.java
+++ java/com/craftinginterpreters/tool/GenerateAst.java
@@ -10,7 +10,7 @@ public class GenerateAst {
   public static void main(String[] args) throws IOException {
     if (args.length != 1) {
       System.err.println("Usage: generate_ast <output directory>");
-      System.exit(1);
+      System.exit(64);
     }
     String outputDir = args[0];
 //> call-define-ast
