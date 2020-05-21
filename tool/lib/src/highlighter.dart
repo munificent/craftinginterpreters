fc8e1ff528578439c89a5e14a9608c7eed90a5ff
Robert Nystrom <robert@stuffwithstuff.com>
2020-05-21 08:00:02 -0700

    Much more support for C snippets.
    - Add in the markers for typedefs, structs, etc.
    - Add a lot of C features to the syntax highlighter.

--- tool/lib/src/highlighter.dart
+++ tool/lib/src/highlighter.dart
@@ -1,3 +1,4 @@
+import 'package:charcode/ascii.dart';
 import 'package:string_scanner/string_scanner.dart';
 
 /// Match these first because they apply smarter styles in certain contexts.
@@ -7,20 +8,41 @@ final _packagePattern = RegExp(r"package (\w+(\.\w+)*);");
 
 final _annotationPattern = RegExp(r"@[a-zA-Z_][a-zA-Z0-9_]*");
 final _identifierPattern = RegExp(r"[a-zA-Z_][a-zA-Z0-9_]*");
+final _integerPattern = RegExp(r"[0-9]+L?");
 final _lineCommentPattern = RegExp(r"//.*");
-final _operatorPattern = RegExp(r"[(){}[\]!+\-/*:;.,|?=]+");
+final _operatorPattern = RegExp(r"[(){}[\]!+\-/*:;.,|?=<>&]+");
+
+final _includePattern = RegExp(r"(#include)(\s+)(.*)");
+
+// TODO: This includes all trailing whitespace, which I guess is technically
+// correct but looks a little funny. Pygments works this way. If we don't want
+// to match that, a petter regex is "#[a-zA-Z_][a-zA-Z0-9_]*".
+final _preprocessorPattern = RegExp(r"#.*");
+// TODO: Multi-character escapes?
+final _charPattern = RegExp(r"'(\\?.)'");
+final _stringEscapePattern = RegExp(r"\\.");
 
 // TODO: More.
 final _keywords = {
   // Reserved words.
+  "break": "k",
   "case": "k",
+  "const": "k",
+  "continue": "k",
+  "default": "k",
+  "do": "k",
   "else": "k",
+  "enum": "k",
+  "extern": "k",
   "for": "k",
   "if": "k",
   "instanceof": "k",
   "return": "k",
+  "static": "k",
+  "struct": "k",
   "switch": "k",
   "this": "k",
+  "typedef": "k",
   "while": "k",
 
   // Constants.
@@ -45,89 +67,167 @@ final _keywords = {
   "private": "k",
   "protected": "k",
   "public": "k",
+
+  // Built-in names.
+  "NULL": "nb",
+  "this": "nb"
 };
 
 /// Takes a string of source code and returns a block of HTML with spans for
 /// syntax highlighting.
 String formatCode(String language, int length, List<String> lines,
-    [String preClass]) {
-  // TODO: Pass in StringBuffer.
-  var buffer = StringBuffer();
+        [String preClass]) =>
+    Highlighter(length)._highlight(lines, preClass);
 
-  buffer.write("<pre");
-  if (preClass != null) buffer.write(' class="$preClass"');
-  buffer.write(">");
+class Highlighter {
+  final int _lineLength;
+  final StringBuffer _buffer = StringBuffer();
+  StringScanner _scanner;
 
-  // TODO: Temp hack. Munge it some to match the old build output.
-  buffer.write("<span></span>");
+  Highlighter(this._lineLength);
 
-//  # Hack. Markdown seems to discard leading and trailing newlines, so we'll
-//  # add them back ourselves.
-//  leading_newlines = 0
-//  while lines and lines[0].strip() == '':
-//    lines = lines[1:]
-//    leading_newlines += 1
-//
-//  trailing_newlines = 0
-//  while lines and lines[-1].strip() == '':
-//    lines = lines[:-1]
-//    trailing_newlines += 1
-//
-  for (var line in lines) {
-    // TODO: Reuse scanner for all lines?
-    var scanner = StringScanner(line.padRight(length, " "));
+  String _highlight(List<String> lines, String preClass) {
+    // TODO: Pass in StringBuffer.
+    _buffer.write("<pre");
+    if (preClass != null) _buffer.write(' class="$preClass"');
+    _buffer.write(">");
 
-    void token(String type, [String text]) {
-      text ??= scanner.lastMatch.group(0);
-      buffer.write('<span class="$type">$text</span>');
+    // TODO: If we change build to not pass this output through the Markdown
+    // parser, then revisit this.
+    // Hack. Markdown seems to discard leading and trailing newlines, so we'll
+    // add them back ourselves.
+    var leadingNewlines = 0;
+    while (leadingNewlines < lines.length &&
+        lines[leadingNewlines].trim().isEmpty) {
+      leadingNewlines++;
+      _buffer.write("<br>");
     }
 
-    while (!scanner.isDone) {
-      if (scanner.scan("&")) {
-        token("o", "&amp;");
-      } else if (scanner.scan("<")) {
-        token("o", "&lt;");
-      } else if (scanner.scan(">")) {
-        token("o", "&gt;");
-      } else if (scanner.scan(_attributePattern)) {
-        token("o", ".");
-        token("n", scanner.lastMatch.group(1));
-      } else if (scanner.scan(_classPattern)) {
-        token("k", "class");
-        buffer.write(" ");
-        token("nc", scanner.lastMatch.group(1));
-      } else if (scanner.scan(_packagePattern)) {
-        token("k", "package");
-        buffer.write(" ");
-        token("n", scanner.lastMatch.group(1));
-        token("o", ";");
-      } else if (scanner.scan(_annotationPattern)) {
-        token("nd");
-      } else if (scanner.scan(_identifierPattern)) {
-        var identifier = scanner.lastMatch.group(0);
-        token(_keywords[identifier] ?? "n");
-      } else if (scanner.scan(_lineCommentPattern)) {
-        token("c1");
-      } else if (scanner.scan(_operatorPattern)) {
-        token("o");
-      } else {
-        buffer.writeCharCode(scanner.readChar());
-      }
+    var trailingNewlines = 0;
+    while (trailingNewlines < lines.length - leadingNewlines &&
+        lines[lines.length - trailingNewlines - 1].trim().isEmpty) {
+      trailingNewlines++;
     }
 
-    buffer.writeln();
-  }
+    // TODO: Temp hack. Munge it some to match the old build output.
+    _buffer.write("<span></span>");
 
-//  if leading_newlines > 0:
-//    html = html.replace('<pre>', '<pre>' + ('<br>' * leading_newlines))
-//
-//  if trailing_newlines > 0:
-//    html = html.replace('</pre>', ('<br>' * trailing_newlines) + '</pre>')
-//
-//  # Strip off the div wrapper. We just want the <pre>.
+    for (var i = leadingNewlines; i < lines.length - trailingNewlines; i++) {
+      _scanLine(lines[i]);
+    }
+
+    for (var i = 0; i < trailingNewlines; i++) {
+      _buffer.write("<br>");
+    }
+
+    //  # Strip off the div wrapper. We just want the <pre>.
 //  html = html.replace('<div class="codehilite">', '')
 //  html = html.replace('</div>', '')
 
-  buffer.writeln("</pre>");
-  return buffer.toString();
+    _buffer.writeln("</pre>");
+    return _buffer.toString();
+  }
+
+  void _scanLine(String line) {
+    if (line.trim().isEmpty) {
+      _buffer.writeln();
+      return;
+    }
+
+    // TODO: Reuse scanner for all lines?
+    _scanner = StringScanner(line.padRight(_lineLength, " "));
+
+    while (!_scanner.isDone) {
+      if (_scanner.scan(_includePattern)) {
+        _token("cp", _scanner.lastMatch[1]);
+        _write(_scanner.lastMatch[2]);
+        _token("cpf", _scanner.lastMatch[3]);
+      } else if (_scanner.scan(_attributePattern)) {
+        _token("o", ".");
+        _token("n", _scanner.lastMatch[1]);
+      } else if (_scanner.scan(_classPattern)) {
+        _token("k", "class");
+        _buffer.write(" ");
+        _token("nc", _scanner.lastMatch[1]);
+      } else if (_scanner.scan(_packagePattern)) {
+        _token("k", "package");
+        _buffer.write(" ");
+        _token("n", _scanner.lastMatch[1]);
+        _token("o", ";");
+      } else if (_scanner.scan(_annotationPattern)) {
+        _token("nd");
+      } else if (_scanner.scan(_charPattern)) {
+        _token("s", "'${_scanner.lastMatch[1]}'");
+      } else if (_scanner.scan(_identifierPattern)) {
+        var identifier = _scanner.lastMatch[0];
+        _token(_keywords[identifier] ?? "n");
+      } else if (_scanner.scan(_integerPattern)) {
+        _token("mi");
+      } else if (_scanner.scan(_lineCommentPattern)) {
+        _token("c1");
+      } else if (_scanner.scan(_operatorPattern)) {
+        _token("o");
+      } else if (_scanner.scan(_preprocessorPattern)) {
+        _token("cp");
+      } else if (_scanner.scan('"')) {
+        var start = _scanner.position - 1;
+
+        while (!_scanner.isDone) {
+          if (_scanner.scan(_stringEscapePattern)) {
+            if (_scanner.position > start) {
+              // TODO: Something smarter than "-2" if we need multi-character
+              // escapes.
+              _token("s", _scanner.substring(start, _scanner.position - 2));
+            }
+            _token("se");
+            start = _scanner.position;
+          } else if (_scanner.scanChar($double_quote)) {
+            _token("s", _scanner.substring(start, _scanner.position));
+            break;
+          } else {
+            _scanner.position++;
+          }
+        }
+      } else {
+        _writeChar(_scanner.readChar());
+      }
+    }
+
+    _buffer.writeln();
+  }
+
+  void _token(String type, [String text]) {
+    text ??= _scanner.lastMatch[0];
+    _buffer.write('<span class="$type">');
+    _write(text);
+    _buffer.write('</span>');
+  }
+
+  void _write(String string) {
+    for (var i = 0; i < string.length; i++) {
+      _writeChar(string.codeUnitAt(i));
+    }
+  }
+
+  void _writeChar(int char) {
+    switch (char) {
+      case $less_than:
+        _buffer.write("&lt;");
+        break;
+      case $greater_than:
+        _buffer.write("&gt;");
+        break;
+      case $single_quote:
+        _buffer.write("&#39;");
+        break;
+      case $double_quote:
+        _buffer.write("&quot;");
+        break;
+      case $ampersand:
+        _buffer.write("&amp;");
+        break;
+      default:
+        _buffer.writeCharCode(char);
+    }
+  }
 }
