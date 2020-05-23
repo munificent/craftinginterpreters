6a31cfa9bb32f4dcd9f7c17e5a1956418042fe4f
Robert Nystrom <robert@stuffwithstuff.com>
2020-05-23 10:31:00 -0700

    Format ":::language" style code blocks.

--- tool/lib/src/markdown.dart
+++ tool/lib/src/markdown.dart
@@ -8,6 +8,7 @@ String renderMarkdown(String contents) {
   return markdownToHtml(contents.toString(),
       blockSyntaxes: [
         HighlightedCodeBlockSyntax(),
+        TripleColonCodeBlockSyntax(),
       ],
       inlineSyntaxes: [
         // Put inline Markdown code syntax before our smart quotes so that
@@ -55,7 +56,9 @@ class _SmartQuoteSyntax extends InlineSyntax {
     if (before == $dot) return true;
     if (before == $question) return true;
     if (before == $exclamation) return true;
+
     if (after == $comma) return true;
+    if (after == $dot) return true;
 
     // Default to left.
     return false;
@@ -100,30 +103,75 @@ class HighlightedCodeBlockSyntax extends BlockSyntax {
     // The Markdown tests expect a trailing newline.
     childLines.add("");
 
-    String code;
-    if (language == "text") {
-      // Don't syntax highlight text.
-      code = escapeHtml(childLines.join("\n"));
+    return _formatCodeLines(language, childLines);
+  }
+}
 
-      // TODO: The Markdown/Pygments puts a pointless empty span at the
-      // beginning. Remove this when not trying to match that.
-      code = "<span></span>$code";
-    } else {
-      // TODO: Find a cleaner way to handle this. Maybe move the trailing
-      // newline code into `insertSnippet()`?
-      // Remove the trailing empty line so that `formatCode()` doesn't put a
-      // <br> at the end.
-      childLines.removeLast();
-      code = formatCode(language, 72, childLines);
+
+/// Custom code block formatter that uses our syntax highlighter.
+///
+/// Recognizes Python Markdown's ":::language" syntax.
+// TODO: Remove this when those code blocks have been migrated to use fences.
+class TripleColonCodeBlockSyntax extends BlockSyntax {
+  static final _startPattern = RegExp(r'^( *):::(.*)$');
+  static final _indentationPattern = RegExp(r'^( *)(.+)$');
+
+  RegExp get pattern => _startPattern;
+
+  bool canParse(BlockParser parser) =>
+      pattern.firstMatch(parser.current) != null;
+
+  List<String> parseChildLines(BlockParser parser, [String indentation]) {
+    var childLines = <String>[];
+    parser.advance();
+
+    while (!parser.isDone) {
+      // Stop when we hit a non-empty line whose indentation is less than the
+      // ":::" line.
+      var match = _indentationPattern.firstMatch(parser.current);
+      if (match != null && match[1].length < indentation.length) break;
+
+      childLines.add(parser.current);
+      parser.advance();
     }
 
-    var element = Element.text("pre", code);
-
-    // TODO: Remove this when no longer trying to match the existing output.
-    // Wrap in codehilite div.
-    element = Element("div", [element]);
-    element.attributes["class"] = "codehilite";
-
-    return element;
+    return childLines;
   }
+
+  Node parse(BlockParser parser) {
+    // Get the syntax identifier, if there is one.
+    var match = pattern.firstMatch(parser.current);
+    var indentation = match[1];
+    var language = match[2];
+
+    var childLines = parseChildLines(parser, indentation);
+    return _formatCodeLines(language, childLines);
+  }
+}
+
+Element _formatCodeLines(String language, List<String> childLines) {
+  String code;
+  if (language == "text") {
+    // Don't syntax highlight text.
+    code = escapeHtml(childLines.join("\n"));
+
+    // TODO: The Markdown/Pygments puts a pointless empty span at the
+    // beginning. Remove this when not trying to match that.
+    code = "<span></span>$code";
+  } else {
+    // TODO: Find a cleaner way to handle this. Maybe move the trailing
+    // newline code into `insertSnippet()`?
+    // Remove the trailing empty line so that `formatCode()` doesn't put a
+    // <br> at the end.
+    childLines.removeLast();
+    code = formatCode(language, 72, childLines);
+  }
+
+  var element = Element.text("pre", code);
+
+  // TODO: Remove this when no longer trying to match the existing output.
+  // Wrap in codehilite div.
+  element = Element("div", [element]);
+  element.attributes["class"] = "codehilite";
+  return element;
 }
