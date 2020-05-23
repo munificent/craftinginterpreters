e49f082779e7301bc38e3ecbb153f8cfa76ec700
Robert Nystrom <robert@stuffwithstuff.com>
2020-05-23 11:47:47 -0700

    Build a more structured syntax highlighting system.
    This should make it easier to define language-specific rules and flush
    out the remaining inconsistencies.

--- tool/lib/src/highlighter.dart
+++ tool/lib/src/highlighter.dart
@@ -1,138 +1,6 @@
 import 'package:charcode/ascii.dart';
 import 'package:string_scanner/string_scanner.dart';
 
-/// Match these first because they apply smarter styles in certain contexts.
-final _attributePattern = RegExp(r"\.([a-zA-Z_][a-zA-Z0-9_]*)");
-final _classPattern = RegExp(r"class (\w+)");
-final _packagePattern = RegExp(r"package (\w+(\.\w+)*);");
-
-final _annotationPattern = RegExp(r"@[a-zA-Z_][a-zA-Z0-9_]*");
-final _floatPattern = RegExp(r"[0-9]+\.[0-9]+f?");
-final _identifierPattern = RegExp(r"[a-zA-Z_][a-zA-Z0-9_]*");
-final _integerPattern = RegExp(r"[0-9]+L?");
-final _labelPattern = RegExp(r"([a-zA-Z_][a-zA-Z0-9_]*)(\s*)(:)");
-final _lineCommentPattern = RegExp(r"//.*");
-// TODO: Allowing a space in here will produce visually identical output but
-// collapse to fewer spans in cases like ") + (".
-final _operatorPattern = RegExp(r"[(){}[\]!+\-/*:;.,|?=<>&]+");
-
-final _includePattern = RegExp(r"(#include)(\s+)(.*)");
-
-// TODO: This includes all trailing whitespace, which I guess is technically
-// correct but looks a little funny. Pygments works this way. If we don't want
-// to match that, a petter regex is "#[a-zA-Z_][a-zA-Z0-9_]*".
-final _preprocessorPattern = RegExp(r"#.*");
-// TODO: Multi-character escapes?
-final _charPattern = RegExp(r"'(\\?.)'");
-final _stringEscapePattern = RegExp(r"\\.");
-
-final _sharedKeywords = {
-  // Reserved words.
-  "break": "k",
-  "case": "k",
-  "const": "k",
-  "continue": "k",
-  "default": "k",
-  "do": "k",
-  "else": "k",
-  "enum": "k",
-  "for": "k",
-  "if": "k",
-  "return": "k",
-  "static": "k",
-  "struct": "k",
-  "switch": "k",
-  "while": "k",
-
-  // Types.
-  "bool": "k",
-  "char": "k",
-  "double": "k",
-  "int": "k",
-  "void": "k",
-};
-
-final _cKeywords = {
-  ..._sharedKeywords,
-
-  // Reserved words.
-  "extern": "k",
-  "sizeof": "k",
-  "typedef": "k",
-
-  // Types.
-  "FILE": "k",
-  "size_t": "k",
-  "uint8_t": "k",
-  "uint16_t": "k",
-  "uint32_t": "k",
-
-  // Built-in names.
-  "false": "nb",
-  "NULL": "nb",
-  "true": "nb",
-};
-
-final _javaKeywords = {
-  ..._sharedKeywords,
-
-  // Reserved words.
-  "abstract": "k",
-  "catch": "k",
-  "class": "k",
-  "extends": "k",
-  "final": "k",
-  "implements": "k",
-  "import": "k",
-  "instanceof": "k",
-  "interface": "k",
-  "new": "k",
-  "private": "k",
-  "protected": "k",
-  "public": "k",
-  "this": "k",
-  "throw": "k",
-
-  // Constants.
-  "false": "kc",
-  "null": "kc",
-  "true": "kc",
-
-  // Types.
-  "boolean": "k",
-};
-
-final _loxKeywords = {
-  ..._sharedKeywords,
-
-  // Reserved words.
-  "fun": "k",
-  "print": "k",
-  "var": "k",
-
-  // Built-in names.
-  // TODO: Make this a keyword? It is in Java.
-  "this": "nb"
-};
-
-final _languages = {
-  "c": _cKeywords,
-  // TODO: Add C++ support.
-  "c++": _cKeywords,
-  "java": _javaKeywords,
-  // TODO: Add JS support.
-  "js": _sharedKeywords,
-  // TODO: Add Lisp support.
-  "lisp": _sharedKeywords,
-  "lox": _loxKeywords,
-  // TODO: Add Lua support.
-  "lua": _sharedKeywords,
-  // TODO: Add Python support.
-  "python": _sharedKeywords,
-  // TODO: Add Ruby support.
-  "ruby": _sharedKeywords
-};
-
 /// Takes a string of source code and returns a block of HTML with spans for
 /// syntax highlighting.
 ///
@@ -167,10 +35,10 @@ class Highlighter {
   final int _lineLength;
   final StringBuffer _buffer;
   StringScanner _scanner;
-  final Map<String, String> _keywords;
+  final Language _language;
 
   Highlighter(this._buffer, String language, this._lineLength)
-      : _keywords =
+      : _language =
             _languages[language] ?? (throw "Unknown language '$language'.");
 
   String _highlight(List<String> lines) {
@@ -215,91 +83,15 @@ class Highlighter {
     _scanner = StringScanner(line.padRight(_lineLength, " "));
 
     while (!_scanner.isDone) {
-      if (_scanner.scan(_includePattern)) {
-        _token("cp", _scanner.lastMatch[1]);
-        _write(_scanner.lastMatch[2]);
-        _token("cpf", _scanner.lastMatch[3]);
-      } else if (_scanner.scan(_attributePattern)) {
-        _token("o", ".");
-        _token("n", _scanner.lastMatch[1]);
-      } else if (_scanner.scan(_classPattern)) {
-        _token("k", "class");
-        _buffer.write(" ");
-        _token("nc", _scanner.lastMatch[1]);
-      } else if (_scanner.scan(_packagePattern)) {
-        _token("k", "package");
-        _buffer.write(" ");
-        _token("n", _scanner.lastMatch[1]);
-        _token("o", ";");
-      } else if (_scanner.scan(_annotationPattern)) {
-        _token("nd");
-      } else if (_scanner.scan(_charPattern)) {
-        _token("s", "'${_scanner.lastMatch[1]}'");
-      } else if (_scanner.scan(_floatPattern)) {
-        _token("mf");
-      } else if (_scanner.scan(_labelPattern)) {
-        var name = _scanner.lastMatch[1];
-        var space = _scanner.lastMatch[2];
-        var colon = _scanner.lastMatch[3];
-        _token(_keywords[name] ?? "nl", name);
-        // TODO: Allowing space here means that this incorrectly parses an
-        // identifier before a conditional operator as a label name. Pygments
-        // does this wrong so this matches that. Once we aren't trying to match
-        // exactly, remove the (\s*) from the regex to fix that.
-        _write(space);
-        _token("o", colon);
-      } else if (_scanner.scan(_identifierPattern)) {
-        var identifier = _scanner.lastMatch[0];
-        var type = _keywords[identifier] ?? "n";
-
-        // Capitalized identifiers are treated specially in Lox.
-        // TODO: Do something less hacky.
-        if (_keywords == _loxKeywords &&
-            identifier.codeUnitAt(0) >= $A &&
-            identifier.codeUnitAt(0) <= $Z) {
-          type = "nc";
+      var found = false;
+      for (var rule in _language.rules) {
+        if (rule.apply(this)) {
+          found = true;
+          break;
         }
-
-        _token(type);
-      } else if (_scanner.scan(_integerPattern)) {
-        _token("mi");
-      } else if (_scanner.scan(_lineCommentPattern)) {
-        _token("c1");
-      } else if (_scanner.scan(_operatorPattern)) {
-        _token("o");
-      } else if (_scanner.scan(_preprocessorPattern)) {
-        _token("cp");
-      } else if (_scanner.scan('"')) {
-        var start = _scanner.position - 1;
-
-        while (!_scanner.isDone) {
-          if (_scanner.scan(_stringEscapePattern)) {
-            if (_scanner.position > start) {
-              // TODO: Something smarter than "-2" if we need multi-character
-              // escapes.
-              _token("s", _scanner.substring(start, _scanner.position - 2));
-            }
-            _token("se");
-            start = _scanner.position;
-          } else if (_scanner.scanChar($double_quote)) {
-            _token("s", _scanner.substring(start, _scanner.position));
-            break;
-          } else {
-            _scanner.position++;
-          }
-        }
-      } else if (_scanner.scan("\\")) {
-        // TODO: Pygments doesn't handle backslashes in multi-line defines, so
-        // report the same error here. Remove this when not trying to match
-        // that.
-        _token("err", _scanner.lastMatch[0]);
-      } else if (_scanner.scan("→")) {
-        // TODO: Just leave this as plain text once we aren't trying to match
-        // Pygments.
-        _token("err", _scanner.lastMatch[0]);
-      } else {
-        _writeChar(_scanner.readChar());
       }
+
+      if (!found) _writeChar(_scanner.readChar());
     }
 
     _buffer.writeln();
@@ -340,3 +132,228 @@ class Highlighter {
     }
   }
 }
+
+/// Defines the syntax rules for a single programming language.
+class Language {
+  final Map<String, String> words = {};
+  final List<Rule> rules;
+
+  Language(
+      {String keywords,
+      String constants,
+      String names,
+      Map<String, String> other,
+      List<Rule> rules})
+      // TODO: Allow omitting rules for languages that aren't supported yet.
+      : rules = rules ?? const [] {
+    keywordType(String wordList, String type) {
+      if (wordList == null) return;
+      for (var word in wordList.split(" ")) {
+        words[word] = type;
+      }
+    }
+
+    keywordType(keywords, "k");
+    keywordType(constants, "kc");
+    keywordType(names, "nb");
+    if (other != null) words.addAll(other);
+  }
+}
+
+abstract class Rule {
+  final RegExp pattern;
+
+  factory Rule(String pattern, String tokenType) =>
+      SimpleRule(pattern, tokenType);
+
+  factory Rule.capture(String pattern, List<String> tokenTypes) =>
+      CaptureRule(pattern, tokenTypes);
+
+  Rule._(String pattern) : pattern = RegExp(pattern);
+
+  bool apply(Highlighter highlighter) {
+    if (!highlighter._scanner.scan(pattern)) return false;
+    applyRule(highlighter);
+    return true;
+  }
+
+  void applyRule(Highlighter highlighter);
+}
+
+/// Parses a single regex and outputs the entire matched text as a single token
+/// with the given [tokenType].
+class SimpleRule extends Rule {
+  final String tokenType;
+
+  SimpleRule(String pattern, this.tokenType) : super._(pattern);
+
+  void applyRule(Highlighter highlighter) {
+    highlighter._token(tokenType);
+  }
+}
+
+/// Parses a single regex where each capture group has a corresponding token
+/// type. If the type is `""` for some group, the matched string text is output
+/// as plain text.
+class CaptureRule extends Rule {
+  final List<String> tokenTypes;
+
+  CaptureRule(String pattern, this.tokenTypes) : super._(pattern);
+
+  void applyRule(Highlighter highlighter) {
+    var match = highlighter._scanner.lastMatch;
+    for (var i = 0; i < tokenTypes.length; i++) {
+      var type = tokenTypes[i];
+      if (type.isNotEmpty) {
+        highlighter._token(type, match[i + 1]);
+      } else {
+        highlighter._write(match[i + 1]);
+      }
+    }
+  }
+}
+
+/// Parses string literals and the escape codes inside them.
+class StringRule extends Rule {
+  static final _escapePattern = RegExp(r"\\.");
+
+  StringRule() : super._('"');
+
+  void applyRule(Highlighter highlighter) {
+    var scanner = highlighter._scanner;
+    var start = scanner.position - 1;
+
+    while (!scanner.isDone) {
+      if (scanner.scan(_escapePattern)) {
+        if (scanner.position > start) {
+          // TODO: Something smarter than "-2" if we need multi-character
+          // escapes.
+          highlighter._token(
+              "s", scanner.substring(start, scanner.position - 2));
+        }
+        highlighter._token("se");
+        start = scanner.position;
+      } else if (scanner.scanChar($double_quote)) {
+        highlighter._token("s", scanner.substring(start, scanner.position));
+        break;
+      } else {
+        scanner.position++;
+      }
+    }
+  }
+}
+
+/// Parses an identifier and resolves keywords for their token type.
+class IdentifierRule extends Rule {
+  IdentifierRule() : super._(r"[a-zA-Z_][a-zA-Z0-9_]*");
+
+  void applyRule(Highlighter highlighter) {
+    var identifier = highlighter._scanner.lastMatch[0];
+    var type = highlighter._language.words[identifier] ?? "n";
+
+    // Capitalized identifiers are treated specially in Lox.
+    // TODO: Do something less hacky.
+    if (highlighter._language == _languages["lox"] &&
+        identifier.codeUnitAt(0) >= $A &&
+        identifier.codeUnitAt(0) <= $Z) {
+      type = "nc";
+    }
+
+    highlighter._token(type);
+  }
+}
+
+// TODO: This is pretty hacky. The Pygments lexers for C and Java handle
+// colons differently. Probably need to fork this.
+/// Parses an identifier followed by a colon and treats it as a label or a
+/// keyword followed by a ":" as needed.
+class LabelRule extends Rule {
+  LabelRule() : super._(r"([a-zA-Z_][a-zA-Z0-9_]*)(\s*)(:)");
+
+  void applyRule(Highlighter highlighter) {
+    var name = highlighter._scanner.lastMatch[1];
+    var space = highlighter._scanner.lastMatch[2];
+    var colon = highlighter._scanner.lastMatch[3];
+    highlighter._token(highlighter._language.words[name] ?? "nl", name);
+    // TODO: Allowing space here means that this incorrectly parses an
+    // identifier before a conditional operator as a label name. Pygments
+    // does this wrong so this matches that. Once we aren't trying to match
+    // exactly, remove the (\s*) from the regex to fix that.
+    highlighter._write(space);
+    highlighter._token("o", colon);
+  }
+}
+
+final _languages = {
+  "c": Language(
+      keywords: "bool break case char const continue default do double else "
+          "enum extern FILE for if int return size_t sizeof static struct "
+          "switch typedef uint16_t uint32_t uint8_t void while",
+      names: "false NULL TRUE",
+      rules: _rules),
+  // TODO: Add C++ support.
+  "c++": Language(),
+  "java": Language(
+      keywords: "abstract assert boolean break byte case catch char class "
+          "const continue default do double else enum extends final finally "
+          "float for goto if implements import instanceof int interface long "
+          "native new package private protected public return short static "
+          "strictfp super switch synchronized this throw throws transient try "
+          "void volatile while",
+      constants: "false null true",
+      rules: _rules),
+  // TODO: Add JS support.
+  "js": Language(),
+  // TODO: Add Lisp support.
+  "lisp": Language(),
+  // TODO: Make `this` a keyword? It is in Java.
+  "lox": Language(
+      keywords: "and class else false fun for if nil or print return super "
+          "true var while",
+      names: "this",
+      rules: _rules),
+  // TODO: Add Lua support.
+  "lua": Language(),
+  // TODO: Add Python support.
+  "python": Language(),
+  // TODO: Add Ruby support.
+  "ruby": Language(),
+};
+
+final _rules = [
+  Rule.capture(r"(\.)([a-zA-Z_][a-zA-Z0-9_]*)", ["o", "n"]), // Attribute.
+
+  /// Match these first because they apply smarter styles in certain contexts.
+  Rule.capture(r"(class)(\s+)(\w+)", ["k", "", "nc"]), // Class.
+  Rule.capture(
+      r"(package)(\s+)(\w+(?:\.\w+)*)(;)", ["k", "", "n", "o"]), // Package.
+
+  Rule(r"@[a-zA-Z_][a-zA-Z0-9_]*", "nd"), // Annotation.
+  // TODO: Multi-character escapes?
+  Rule(r"'\\?.'", "s"), // Character.
+  StringRule(),
+
+  Rule(r"[0-9]+\.[0-9]+f?", "mf"), // Float.
+  Rule(r"[0-9]+L?", "mi"), // Integer.
+
+  Rule(r"//.*", "c1"), // Line comment.
+  // TODO: Allowing a space in here will produce visually identical output but
+  // collapse to fewer spans in cases like ") + (".
+  Rule(r"[(){}[\]!+\-/*:;.,|?=<>&]+", "o"), // Operator.
+
+  Rule.capture(r"(#include)(\s+)(.*)", ["cp", "", "cpf"]), // Include.
+  // TODO: This includes all trailing whitespace, which I guess is technically
+  // correct but looks a little funny. Pygments works this way. If we don't want
+  // to match that, a better regex is "#[a-zA-Z_][a-zA-Z0-9_]*".
+  Rule(r"#.*", "cp"), // Preprocessor.
+
+  IdentifierRule(),
+
+  // TODO: Pygments doesn't handle backslashes in multi-line defines, so
+  // report the same error here. Remove this when not trying to match
+  // that.
+  Rule(r"\\", "err"),
+  // TODO: Just leave this as plain text once we aren't trying to match
+  // Pygments.
+  Rule(r"→", "err"),
+];
