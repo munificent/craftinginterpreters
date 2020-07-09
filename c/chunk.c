148630e89baf07006d0c40cf135c39f64258d6f5
Robert Nystrom <robert@stuffwithstuff.com>
2020-07-08 21:02:43 -0700

    Make memory management macro/function parameters consistent.
    Fix #722.

--- c/chunk.c
+++ c/chunk.c
@@ -42,10 +42,10 @@ void writeChunk(Chunk* chunk, uint8_t byte, int line) {
   if (chunk->capacity < chunk->count + 1) {
     int oldCapacity = chunk->capacity;
     chunk->capacity = GROW_CAPACITY(oldCapacity);
-    chunk->code = GROW_ARRAY(chunk->code, uint8_t,
+    chunk->code = GROW_ARRAY(uint8_t, chunk->code,
         oldCapacity, chunk->capacity);
 //> write-chunk-line
-    chunk->lines = GROW_ARRAY(chunk->lines, int,
+    chunk->lines = GROW_ARRAY(int, chunk->lines,
         oldCapacity, chunk->capacity);
 //< write-chunk-line
   }
