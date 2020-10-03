232108093cd4c88e9515ced093c15af7260c4dbe
Robert Nystrom <robert@stuffwithstuff.com>
2020-10-02 20:50:15 -0700

    Abort if the gray stack allocation fails.
    Fix #828.

--- c/memory.c
+++ c/memory.c
@@ -72,6 +72,10 @@ void markObject(Obj* object) {
     vm.grayCapacity = GROW_CAPACITY(vm.grayCapacity);
     vm.grayStack = realloc(vm.grayStack,
                            sizeof(Obj*) * vm.grayCapacity);
+//> exit-gray-stack
+
+    if (vm.grayStack == NULL) exit(1);
+//< exit-gray-stack
   }
 
   vm.grayStack[vm.grayCount++] = object;
