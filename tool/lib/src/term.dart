f68007035ee9c55337e6cf52b254ee06596bef90
Robert Nystrom <robert@stuffwithstuff.com>
2020-11-18 21:06:50 -0800

    Generate separate word count without code.

--- tool/lib/src/term.dart
+++ tool/lib/src/term.dart
@@ -1,15 +1,17 @@
 /// Utilities for printing to the terminal.
 import 'dart:io';
 
+final _cyan = _ansi('\u001b[36m');
+final _gray = _ansi('\u001b[1;30m');
 final _green = _ansi('\u001b[32m');
 final _magenta = _ansi('\u001b[35m');
 final _pink = _ansi('\u001b[91m');
 final _red = _ansi('\u001b[31m');
 final _yellow = _ansi('\u001b[33m');
-final _gray = _ansi('\u001b[1;30m');
 final _none = _ansi('\u001b[0m');
 final _resetColor = _ansi('\u001b[39m');
 
+String cyan(Object message) => "$_cyan$message$_none";
 String gray(Object message) => "$_gray$message$_none";
 String green(Object message) => "$_green$message$_resetColor";
 String magenta(Object message) => "$_magenta$message$_resetColor";
